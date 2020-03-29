495e08cd21ee7158b347e1298d118d0449493c09
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2020-03-29 15:53:19 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("Ingrese el primer número: "))
+c = int(input("Ingrese el segundo número: "))
+
+producto = a * c
+print("El producto de los números {} y {} es: {}".format(a, c, producto))
+
+doble_a= a * 2
+print("El doble del primer número ingresado, es decir, del {}, es: {}".format(a, doble_a))
+
